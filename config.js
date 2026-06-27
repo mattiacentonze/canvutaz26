6384f7dab1934d517b6e1a90615cb6ced8a052cd
Mattia <mattiacentonze@pm.me>
2026-06-28 00:36:26 +0200

    update config

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// Configurazione USA26 Roadtrip Planner v2.
+// Configurazione USA26 Roadtrip Planner.
 // Il sito è in sola lettura: il piano si modifica dal Google Sheet condiviso solo con voi tre.
 window.USA26_CONFIG = {
   // URL della Web App Google Apps Script in sola lettura, es. https://script.google.com/macros/s/XXXX/exec
-  APPS_SCRIPT_URL: '',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1ewrpTu6IhVrhPFkMxOe5cRiwVqHCOUo1FSmJpEdPlnpbKCTwtfOtPXxX/exec',
 
   // URL del Google Sheet privato, condiviso manualmente solo con gli amici editor.
-  GOOGLE_SHEET_URL: '',
+  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1qo5XI_DLkNIviT3IcIbZAS4wk9dnkPhCHop5txiABSQ/edit',
 
   // Lascia false: evita scritture pubbliche dal sito.
   PUBLIC_EDIT_MODE: false,
   READ_ONLY_MODE: true,
 
-  TRIP_NAME: 'USA26 Roadtrip'
+  TRIP_NAME: 'CANVUTAZ26 Roadtrip'
 };
