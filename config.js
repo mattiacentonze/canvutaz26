5204a1d3a3f2a2001434a705a5eca10e6f73efcd
Mattia <mattiacentonze@pm.me>
2026-06-28 01:50:25 +0200

    update apps script

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Copia questo file come config.js e inserisci i tuoi link.
 window.USA26_CONFIG = {
   // URL della Web App Google Apps Script in sola lettura, es. https://script.google.com/macros/s/XXXX/exec
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1ewrpTu6IhVrhPFkMxOe5cRiwVqHCOUo1FSmJpEdPlnpbKCTwtfOtPXxX/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1WC3ybRGMjXdVjurhcoGBeyf-4B4ir1rWcuQ_9l5j4S8xcAojJGnXN7WV/exec',
 
   // URL del Google Sheet privato, condiviso manualmente solo con gli amici editor.
   GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1cnic2mdTvJ2UD8Cpy3hjOGG-mAiRK2w0Hg-Ox4Ol7sE/edit',
