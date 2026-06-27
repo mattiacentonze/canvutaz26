2e27c54d0b9340f6e9409047608fd34d2ff33a52
Mattia <mattiacentonze@pm.me>
2026-06-28 01:49:29 +0200

    update config

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.USA26_CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1ewrpTu6IhVrhPFkMxOe5cRiwVqHCOUo1FSmJpEdPlnpbKCTwtfOtPXxX/exec',
 
   // URL del Google Sheet privato, condiviso manualmente solo con gli amici editor.
-  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1dlElDyvDph2Ibc-RnpmXV-VS3mPix7zU/edit',
+  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1cnic2mdTvJ2UD8Cpy3hjOGG-mAiRK2w0Hg-Ox4Ol7sE/edit',
 };
