4747a92bb8ca9e34abcff4c7ec6bb63020ad99d7
mattiacentonze <mattiacentonze@users.noreply.github.com>
2026-06-28 02:31:57 +0200

    d

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Copia questo file come config.js e inserisci i tuoi link.
 window.USA26_CONFIG = {
   // URL della Web App Google Apps Script in sola lettura, es. https://script.google.com/macros/s/XXXX/exec
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1WC3ybRGMjXdVjurhcoGBeyf-4B4ir1rWcuQ_9l5j4S8xcAojJGnXN7WV/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/1c35TiobI05aqJTriPhdBGN3Px3tSUIIldHIgAX0D47YuDob0h07mC6II/exec',
 
   // URL del Google Sheet privato, condiviso manualmente solo con gli amici editor.
-  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1cnic2mdTvJ2UD8Cpy3hjOGG-mAiRK2w0Hg-Ox4Ol7sE/edit',
+  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1iVNIX-nnxr2rfs2V-vp7xQop9YIBeHqP4s21EpNOI3k/edit',
 };
